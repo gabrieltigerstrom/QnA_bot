2309e5c4b076084d75b35592421ad852b0b31c13
Gabriel Tigerström <gtig@kth.se>
2018-04-30 16:36:31 +0200

    Print if success

--- indexer/indexer.py
+++ indexer/indexer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 from datetime import datetime
 from elasticsearch import Elasticsearch
+
 es = Elasticsearch([{'host': 'elasticsearch', 'port': 9200}])
 if not es.ping():
     raise ValueError("Connection failed")
+else:
+    print("Connection estabished!")
