4884a6cfc1da2935e36dfaaaa284423a5530e3c7
TonyHsu <sunprince12014@gmail.com>
2018-05-13 23:08:56 +0200

    [Performance] Speed up - bulk indexing

--- indexer/indexer.py
+++ indexer/indexer.py
@@ -16,12 +16,18 @@ if __name__ == "__main__":
     parser.add_argument('doc_type',type=str,choices=['answers','questions'])
     parser.add_argument('--mark',default='main',type=str,help='for DEBUG')
     parser.add_argument('--size',type=int,help='for sub-test')
+    parser.add_argument('--nobulk',action='store_true')
+    parser.add_argument('--bulksize',type=int,default=2000)
+    parser.add_argument('--local',action='store_true',help='for local test')
 
     args = parser.parse_args()
     data_path = "resources/{}".format(args.forum)
     random.seed(101)
 
-    connections.create_connection(hosts=['elasticsearch'])
+    if args.local:
+        connections.create_connection(hosts=['localhost'])
+    else:
+        connections.create_connection(hosts=['elasticsearch'])
 
     index = '{forum}_{doc_type}_{mark}'.format(**vars(args))
     with open('{}/{forum}_{doc_type}.json'.format(data_path,**vars(args))) as fin:
@@ -35,11 +41,19 @@ if __name__ == "__main__":
         else:
             Question.init(index=index)
 
+
+    # Start indexing
     if args.doc_type == 'answers':
-        for k in tqdm(data):
-            process_answer(k,data[k],index)
-        Answer._doc_type.refresh()
+        if args.nobulk:
+            for k in tqdm(data):
+                process_answer(k,data[k],index)
+            Answer._doc_type.refresh()
+        else:
+            bulk_process_answer(data,index,connections.get_connection(),args.bulksize)
     else:
-        for k in tqdm(data):
-            process_question(k,data[k],index)
-        Question._doc_type.refresh()
+        if args.nobulk:
+            for k in tqdm(data):
+                process_question(k,data[k],index)
+            Question._doc_type.refresh()
+        else:
+            bulk_process_question(data,index,connections.get_connection(),args.bulksize)
--- indexer/question.py
+++ indexer/question.py
@@ -1,4 +1,6 @@
 from elasticsearch_dsl import DocType, Date, Integer, Nested, analyzer, InnerDoc, Keyword, Text
+from elasticsearch.helpers import bulk
+from tqdm import tqdm
 from marcos import *
 
 #FIXME: Don't why I cannot get rankinfo.viewcnt, so not packed here
@@ -17,6 +19,7 @@ class Question(DocType):
     related = Integer()
     viewcnt = Integer()
     score = Integer()
+    dup = Integer()
     favoritecnt = Integer()
     acceptedAnswer = Integer()
     answers = Integer()
@@ -25,6 +28,38 @@ class Question(DocType):
 
     def save(self,**kwargs):
         return super().save(** kwargs)
+
+def bulk_process_question(questions,index,client,bulksize):
+    """
+    Args:
+        questions: dict., json dump
+        index: str
+        client: connections() object
+        bulksize: int., suggested range: 1K - 5K (depend on your RAM)
+    """
+    data = [ {
+        '_type': 'doc',
+        '_index': index,
+        '_id': int(k),
+        '_source':{
+            'title': q['title'],
+            'tags': q['tags'],
+            'userid': q['userid'] if q['userid'] else None,
+            'related': q['related'] if len(q['related']) else None,
+            'dup': q['dups'] if len(q['dups']) else None,
+            'viewcnt': q['viewcount'],
+            'score': q['score'],
+            'favoritecnt': q['favoritecount'],
+            'creationdate': q['creationdate'],
+            'body': q['body'],
+            'answers': q['answers'] if len(q['answers']) else None,
+            'acceptedAnswer': q['acceptedanswer'] if q['acceptedanswer'] else None
+        } } for k,q in questions.items()]
+    
+    data = [data[i:i+bulksize] for i in range(0,len(data),bulksize)]
+    
+    for d in tqdm(data):
+        bulk(client,d,raise_on_error=True,refresh=True)
 
 def process_question(k,q,index):
     """
@@ -38,16 +73,15 @@ def process_question(k,q,index):
         tags=q['tags'],
         userid=int(q['userid']),
         related=q['related'],
-        dup = q['dups'],
         viewcnt=q['viewcount'],
+        dup = q['dups'],
         score=q['score'],
         favoritecnt=q['favoritecount'],
         creationdate=q['creationdate'],
         body = q['body'],
-        answers = q['answers']
+        answers = q['answers'],
+        acceptedAnswer = q['acceptedanswer'] if q['acceptedanswer'] else None
     )
-    if q['acceptedanswer']:
-        q_obj.acceptedAnswer = q['acceptedanswer']
     # q_obj.rankinfo = RankInfo(viewcnt=q['viewcount'],score=q['score'],favoritecnt=q['favoritecount'])
 
     q_obj.meta.id=int(k)
