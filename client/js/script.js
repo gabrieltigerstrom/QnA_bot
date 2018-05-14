3e1d6ad5c0a3adfb4208c0eda23b6abefa3dc461
Gabriel Tigerström <gtig@kth.se>
2018-05-14 12:35:54 +0200

    Fix client and server

--- client/js/script.js
+++ client/js/script.js
@@ -1,10 +1,14 @@
 $( "#searchform" ).submit(function( event ) {
 	event.preventDefault();
-	let url = "http://localhost:4000/?q=5";
+	let url = "http://localhost:4000/?q=" + $('#searchtext').val();
 	var jqxhr = $.getJSON( url , function(data) {
 		console.log( data );
 	})
-		.done(function() {
+		.done(function(data) {
+
+			console.log(data);
+			//TODO: Check that data contains an answer
+			document.getElementById('result').innerHTML = data.answer;
 			console.log( "Got results" );
 		})
 		.fail(function() {
--- server/server.py
+++ server/server.py
@@ -1,6 +1,13 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qsl
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl.connections import connections
+from elasticsearch_dsl.query import Match
+from elasticsearch_dsl import Search
+from sent_sim import *
 import json
+
+ESConnection = None
 
 class QueryHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -10,18 +17,40 @@ class QueryHandler(BaseHTTPRequestHandler):
         self.end_headers()
         parsed_path = urlparse(self.path)
         print("Path", self.path)
+        print("Connection", ESConnection)
         queries = dict(parse_qsl(parsed_path.query))
         print(queries)
-        self.wfile.write(json.dumps({'q':queries['q']}).encode())
+        print(queries['q'])
+        answer = self.search(queries['q'], "gaming")
+        if answer is not None:
+            self.wfile.write(json.dumps({'answer':answer}).encode())
+        else:
+            self.wfile.write(json.dumps({'error':'no_match'}).encode())
         return
 
+    def search(self, query, forum):
+        q_index = '{}_questions_main'.format(forum)
+        a_index = '{}_answers_main'.format(forum)
+        similar_queries = find_similar_query(query,ESConnection,q_index,1)
+        if similar_queries[0]['acceptedAnswer'] is not None:
+            s = Search(using=ESConnection,index=a_index).query(Match(_id=int(similar_queries[0]['acceptedAnswer'])))
+            res = s.execute()
+            return res['hits']['hits'][0]['_source']['body']
+        else:
+            return None
 
 if __name__ == '__main__':
+    connections.create_connection(hosts=['elasticsearch'])
+    ESConnection = connections.get_connection()
+    
     address, port = '0.0.0.0', 4000
+    connection = "hello"
     server = HTTPServer((address, port), QueryHandler)
+    #server = HTTPServer((address, port), QueryHandler(connections.getConnections()))
     try:
         print("Server was started on {}:{}".format(address, port))
         server.serve_forever()
+
     except KeyboardInterrupt:
         pass
     server.server_close()
